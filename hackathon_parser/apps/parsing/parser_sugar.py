64b85b7434156526c4f242e371516cb3a2a6a58c
AXOT <antonzayats55@gmail.com>
2021-01-26 22:48:35 +0200

    millet, sugar, buckwheat

---
+++ hackathon_parser/apps/parsing/parser_sugar.py
@@ -0,0 +1,145 @@
+from bs4 import BeautifulSoup
+import requests
+from urllib.parse import urljoin
+import re
+
+class Site():
+    
+    def __init__(self, name, url):
+        self.name = name
+        self.url = url
+
+    def __str__(self):
+        return self.name
+
+
+class Item():
+    
+    def __init__(self, price, url, img_url, shop_name, description=None):
+        self.shop_name = shop_name
+        self.img_url = img_url,
+        self.price = price
+        self.url = url
+        self.description = description
+    
+    def __str__(self):
+        return str(self.price)
+    
+    def __dir__(self):
+        return self.price, self.url, self.img_url, self.description
+
+
+SITES = [
+    Site('bigl.ua', 'https://bigl.ua/ua/p775974992-sahar-fasovanonnyj-1kg?click_track_data=.eJwdzrkOgkAUQNF_eTUFa8ahFBFc0AJUgjET1pF1Rtao8d9Fy3ur8wbesmSIe5InoCOkYaRiLAsQhzUPc9r8v6SoClYXAnRsaOMUdIhyWoEAPSvTZs7w4Uh06cZuQqbKb71NPSJaSgXPLNIw0ywNnhD_4MvHyLbwbh3yBpmDiYJAW0Xl6Wxs7VfgkUs-FliRM0N09ndkT_Bz9Cll7XN2dKBfZUUU0e3zBQh5Ous.fWlU35_Ym66AZxnBLN28dlY6EcE'),
+    Site('ukr-produkt.com', 'https://www.ukr-produkt.com/product/sahar/'),
+    Site('thelavka.com', 'https://thelavka.com/ru/grocery/cereals-and-flour/sahar.html')
+]
+
+HEADERS = {'user-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0', 'accept': '*/*'}
+
+def get_html(url, params=None):
+    r = requests.get(url, headers=HEADERS, params=params)
+    return r
+
+
+def parse_lavka(html, url):
+
+    soup = BeautifulSoup(html, 'html.parser')
+    div = soup.find('div', class_='product_card_main card_fragment')
+    url = url
+    shop_name = 'thelavka.com'
+    price = soup.find('span', class_='PricesalesPrice').get_text(strip=True)
+    src = div.find('img', class_='product_card_image').get('src')
+    img_url = urljoin(url, src)
+    description = soup.find('div', class_='product-short-description').get_text(strip=True)
+    item = Item(url=url, price=price, img_url=img_url, description=description, shop_name=shop_name)
+
+    return item
+
+
+def parse_ukr_produkt(html, url):
+
+    soup = BeautifulSoup(html, 'html.parser')
+    div = soup.find('div', class_='summary entry-summary')
+    img = soup.find('img', class_='wp-post-image')
+    div_description = soup.find('div', id='tab-description')
+    url = url
+    shop_name = 'ukr-produkt.com'
+    price = div.find('span', class_='woocommerce-Price-amount amount').get_text(strip=True)
+    img_url = img.get('src')
+    description = ''
+    for elem in div_description.find_all('p'):
+        description += elem.get_text()
+    item = Item(url=url, price=price, img_url=img_url, description=description, shop_name=shop_name)
+
+    return item
+
+
+def parse_bigl(html, url):
+
+    soup = BeautifulSoup(html, 'html.parser')
+    div = soup.find('div', class_='ek-grid ek-grid_indent_l')
+    div_description = soup.find('div', class_='bgl-user-content translate')
+    url = url
+    shop_name = 'bigl.ua'
+    price = div.find('span', class_='bgl-product-price__value').get_text(strip=True)
+    img_url = div.find('img', class_='ek-picture__item').get('src')
+    description_paragraphs = div_description.find_all('p')
+    description = ''
+    for elem in description_paragraphs:
+        description += elem.get_text(strip=True)
+    item = Item(url=url, price=price, img_url=img_url, description=description, shop_name=shop_name)
+
+    return item
+
+
+def check(html, site_name, site_url):
+
+    if site_name == 'thelavka.com':
+        return parse_lavka(html, site_url)
+    if site_name == 'ukr-produkt.com':
+        return parse_ukr_produkt(html, site_url)
+    if site_name == 'bigl.ua':
+        return parse_bigl(html, site_url)
+
+
+def parse():
+    
+    items = []
+    
+    for site in SITES:
+        html = get_html(site.url)
+        if html.status_code == 200:
+            items.append(check(html.text, site.name, site.url))
+        else:
+            print(f'{site.name} error {str(html.status_code)}')
+    
+    for item in items:
+        item.price = re.sub(r',', '.', item.price)
+        item.price = re.sub(r'\D{2,}', '', item.price)
+        item.price = float(item.price)
+
+        if item.price == int(item.price):
+            item.price = int(item.price)
+        
+        item.img_url = list(item.img_url)[0]
+    
+    items.sort(key=lambda x: x.price, reverse=False)
+    
+    return items
+
+
+def test():
+
+    items = parse()
+    for item in items:
+        for elem in item.__dir__():
+            print(f'{elem}\n\n')
+
+
+'''def test():
+    for item in parse():
+        print(item.img_url)'''
+
+
+#test()
--- hackathon_parser/apps/parsing/views.py
+++ hackathon_parser/apps/parsing/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http import Http404, HttpResponseForbidden, HttpResponse, HttpResponseRedirect, JsonResponse
 from django.urls import reverse
 
-from . import parser
+from . import parser_buckwheat, parser_millet, parser_sugar
 
 def index(request):
-    items = parser.parse()
-    return render(request, 'index.html', {'items_list': items})
+    buckwheat = parser_buckwheat.parse()
+    sugar = parser_sugar.parse()
+    millet = parser_millet.parse()
+    return render(request, 'index.html', {'buckwheat_list': buckwheat, 'sugar_list': sugar, 'millet_list': millet})
